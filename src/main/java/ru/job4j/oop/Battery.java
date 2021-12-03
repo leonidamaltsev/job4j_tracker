123a57eb7e1897348a6d82b94438733268481c1a
leonidamaltsev <daf.dof@yandex.ru>
2021-12-04 00:06:04 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,24 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+
+    public void exchange(Battery another) {
+        another.load = another.load + this.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery charger = new Battery(10);
+        Battery another = new Battery(10);
+        charger.exchange(another);
+    }
+}
