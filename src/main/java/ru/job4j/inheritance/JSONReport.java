7cd256562c46930b4a54aba872aac1598b586923
leonidamaltsev <daf.dof@yandex.ru>
2021-12-15 00:17:12 +0300

    4. Переопределение [#174890]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,15 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+
+    @Override
+    public String generate(String name, String body) {
+        return "{" + System.lineSeparator() + "\"name\"" + " : " + name + System.lineSeparator()
+                + "\"body\"" + " : " + body + System.lineSeparator() + "}";
+    }
+
+    public static void main(String[] args) {
+        JSONReport jsonReport = new JSONReport();
+        System.out.println(jsonReport.generate("\"name\"", "\"body\""));
+    }
+}
