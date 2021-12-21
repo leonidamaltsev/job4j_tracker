6be9eca16f411769a8d3ffed6c3c1de76ec9e629
leonidamaltsev <daf.dof@yandex.ru>
2021-12-21 16:26:44 +0300

    3. Массивы и модели. [#174892 #242531]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -6,27 +6,20 @@ public class Library {
         Book name2 = new Book("Clean water", 35);
         Book name3 = new Book("Clean code", 110);
         Book name4 = new Book("BBC", 15);
-        Book[] books = new Book[4];
-        books[0] = name1;
-        books[1] = name2;
-        books[2] = name3;
-        books[3] = name4;
+        Book[] books = {name1, name2, name3, name4};
         for (int index = 0; index < books.length; index++) {
-            Book bk = books[index];
-            System.out.println(bk.getName() + " - " + bk.getPage());
+            System.out.println(books[index].getName() + " - " + books[index].getPage());
         }
         System.out.println("Replace MuMu to BBC");
         books[0] = name4;
         books[3] = name1;
         for (int index = 0; index < books.length; index++) {
-            Book bk = books[index];
-            System.out.println(bk.getName() + " - " + bk.getPage());
+            System.out.println(books[index].getName() + " - " + books[index].getPage());
         }
         System.out.println("Show only book Clean code");
         for (int index = 0; index < books.length; index++) {
-            Book bk = books[index];
-            if (bk.getName().equals("Clean code")) {
-                System.out.println(bk.getName() + " - " + bk.getPage());
+            if (books[index].getName().equals("Clean code")) {
+                System.out.println(books[index].getName() + " - " + books[index].getPage());
             }
         }
     }
