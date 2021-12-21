facf6180116ec39a77e8570fbef545fd0bf25356
leonidamaltsev <daf.dof@yandex.ru>
2021-12-21 16:30:08 +0300

    3. Массивы и модели. [#174892 #242531]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -10,13 +10,11 @@ public class Library {
         for (int index = 0; index < books.length; index++) {
             System.out.println(books[index].getName() + " - " + books[index].getPage());
         }
-        System.out.println("Replace MuMu to BBC");
         books[0] = name4;
         books[3] = name1;
         for (int index = 0; index < books.length; index++) {
             System.out.println(books[index].getName() + " - " + books[index].getPage());
         }
-        System.out.println("Show only book Clean code");
         for (int index = 0; index < books.length; index++) {
             if (books[index].getName().equals("Clean code")) {
                 System.out.println(books[index].getName() + " - " + books[index].getPage());
