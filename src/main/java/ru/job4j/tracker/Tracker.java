cb1b5659396e93be021853370d917656390cdb2e
leonidamaltsev <daf.dof@yandex.ru>
2022-01-03 20:28:26 +0300

    2.6. Реализация класса StartUI. Удаление заявки [#500747]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -57,12 +57,12 @@ public class Tracker {
 
     public boolean delete(int id) {
         int index = indexOf(id);
-        if (index > -1) {
-            System.arraycopy(items, index + 1, items, index, size - (index - 1));
+        boolean result = index != -1;
+        if (result) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
-            return true;
         }
-        return false;
+        return result;
     }
 }
