52e9d0acef6378b5d77922bfe46ebc88dce4b726
leonidamaltsev <daf.dof@yandex.ru>
2022-01-23 21:39:01 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (value[index].equals(key)) {
+                rsl = index;
+            }
+        }
+
+        if (rsl < 0) {
+            throw new ElementNotFoundException("Element not found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] array = new String[] {"1", "2", "3"};
+        try {
+            indexOf(array, "4");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
