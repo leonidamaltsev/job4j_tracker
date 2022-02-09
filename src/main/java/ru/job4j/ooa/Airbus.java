52bc7545e023fe78cdde98b0109aba41612c7116
leonidamaltsev <daf.dof@yandex.ru>
2022-02-09 23:29:36 +0300

    2. final [#334073 #262238]

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -1,7 +1,7 @@
 package ru.job4j.ooa;
 
 public class Airbus extends Aircraft {
-    private static int COUNT_ENGINE = 2;
+    private static final int COUNT_ENGINE = 2;
 
     private String name;
 
@@ -23,8 +23,11 @@ public class Airbus extends Aircraft {
     }
 
     public void printCountEngine() {
-        COUNT_ENGINE = 4;
-        System.out.println("Количество двигателей равно: " + COUNT_ENGINE);
+        if (!name.equals("A380")) {
+            System.out.println("Количество двигателей равно: " + COUNT_ENGINE);
+        } else {
+            System.out.println("Количество двигателей равно: " + 4);
+        }
     }
 
     @Override
