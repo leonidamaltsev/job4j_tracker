a1814c6e3259f2eada671dd5bac993382faf87c5
leonidamaltsev <daf.dof@yandex.ru>
2022-05-23 22:21:17 +0300

    2. Список адресов [#110062]

---
+++ src/main/java/ru/job4j/stream/Address.java
@@ -0,0 +1,40 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+public class Address {
+    private String city;
+
+    private String street;
+
+    private int home;
+
+    private int apartment;
+
+    public Address(String city, String street, int home, int apartment) {
+        this.city = city;
+        this.street = street;
+        this.home = home;
+        this.apartment = apartment;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Address address = (Address) obj;
+        return home == address.home
+                && apartment == address.apartment
+                && Objects.equals(city, address.city)
+                && Objects.equals(street, address.street);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, home, apartment);
+    }
+}
