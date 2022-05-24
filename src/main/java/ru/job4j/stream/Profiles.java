14c0bfd6c32394e8209456f42ef3b7d3e04e95c6
leonidamaltsev <daf.dof@yandex.ru>
2022-05-24 15:40:07 +0300

    2. Список адресов [#110062 #304421]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -6,8 +6,9 @@ import java.util.stream.Collectors;
 
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream()
-                .map(profile -> profile.getAddress())
+        return profiles
+                .stream()
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
 
     }
