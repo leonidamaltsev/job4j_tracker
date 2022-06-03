7dbe1a6500c4c713d4807bc2eaa914ef2050e254
leonidamaltsev <daf.dof@yandex.ru>
2022-06-03 09:04:57 +0300

    8. Перенос методов в Stream API [#279131 #308373]

--- src/main/java/ru/job4j/stream/Card.java
+++ src/main/java/ru/job4j/stream/Card.java
@@ -2,6 +2,14 @@ package ru.job4j.stream;
 
 import java.util.stream.Stream;
 
+enum Suit {
+    Diamonds, Hearts, Spades, Clubs
+}
+
+enum Value {
+    V_6, V_7, V_8
+}
+
 public class Card {
     private Suit suit;
     private Value value;
--- src/main/java/ru/job4j/stream/Suit.java
+++
@@ -1,5 +0,0 @@
-package ru.job4j.stream;
-
-public enum Suit {
-    Diamons, Hearts, Spades, Clubs
-}
