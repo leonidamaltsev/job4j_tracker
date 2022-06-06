1fdd220f3e29095dd1d4b5cd053e850cfa07c879
leonidamaltsev <daf.dof@yandex.ru>
2022-06-06 23:36:53 +0300

    0. Stream API улучшения [#58167]

--- src/main/java/ru/job4j/stream/Student.java
+++ src/main/java/ru/job4j/stream/Student.java
@@ -4,8 +4,11 @@ import java.util.Objects;
 
 public class Student {
     private int score;
-
     private String surname;
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 
     public Student(int score, String surname) {
         this.score = score;
