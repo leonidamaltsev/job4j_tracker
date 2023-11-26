cf54fe1ef99c64a1d4b4f9e3e789d12da225d961
leonidamaltsev <daf.dof@yandex.ru>
2023-11-27 00:05:10 +0300

    1. Интерфейс Queue [#504942]

---
+++ src/main/java/ru/job4j/queue/QueueRunner.java
@@ -0,0 +1,18 @@
+package ru.job4j.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class QueueRunner {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>();
+        queue.offer("first");
+        queue.offer("second");
+        queue.offer("third");
+        queue.offer("fourth");
+
+        System.out.println(queue.peek());
+
+    }
+}
